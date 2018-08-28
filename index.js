1fe13864fe8f919451146a09ee19b5cf9db6030a
Mithun Biswas <42430352+themithunbiswas@users.noreply.github.com>
2018-08-28 17:02:07 +0530

    hello devops added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const app = express();
 app.use((req, res) => {
-        res.send('Hello there !');
+        res.send('Hello DevOps !');
 });
 
 // Starting both http server
